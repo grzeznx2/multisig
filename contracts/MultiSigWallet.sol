665c77088c5eadb101d57aa6f470e5613ab3b2f3
grzeznx2 <grzeznx2@gmail.com>
2023-02-08 18:02:41 +0100

    Add constructor

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -21,4 +21,21 @@ contract MultiSigWallet {
     mapping(uint256 => mapping(address => bool)) approved;
     uint public required;
     Transaction[] public transactions;
+
+    constructor(address[] memory _owners, uint256 _required){
+        require(_owners.length > 0, "At least 1 owner required");
+        require(_required > 0 && _required <= _owners.length, "Invalid required number of owners");
+
+        for(uint256 i; i < _owners.length; i++){
+            address owner = _owners[i];
+
+            require(owner != address(0), "Invalid owner");
+            require(!isOwner[owner], "Owner is not unique");
+            
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        required = _required;
+    }
 }
