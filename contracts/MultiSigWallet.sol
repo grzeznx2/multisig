26c4d416e7051c0bbace0b6b71d36a8aa0f26a47
grzeznx2 <grzeznx2@gmail.com>
2023-02-08 17:45:49 +0100

    Add events

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.9;
 
 
 contract MultiSigWallet {
-  
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint indexed txId);
+    event Approve(uint indexed owner, uint indexed txId);
+    event Revoke(uint indexed owner, uint indexed txId);
+    event Executde(uint indexed txId);
 }
